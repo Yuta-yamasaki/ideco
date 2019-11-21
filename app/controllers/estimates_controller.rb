607c9e2db7d112aa1ea5cb5ade7d2cafa390c8b2
yuta yama <popo9449@gmail.com>
2019-11-21 21:10:50 +0900

    second commit

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -2,4 +2,7 @@ class EstimatesController < ApplicationController
   def index
     
   end
+  def calc
+    @kekka = params["income"]["num1"].to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/sisan",to:"estimates#index" 
+  post "/kekka",to:"estimates#calc"
 end
